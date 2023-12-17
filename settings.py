75d6d87dc5763384e06798c223a8d95a37f4147c
Nurbek878 <nurik878878@yandex.ru>
2023-12-17 22:47:23 +0500

    Send for review

--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@ LOCAL_URL = 'http://localhost/'
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
